3e8c3d31539617f38676e857531a2edce13edee7
Your Name <you@example.com>
2024-03-03 14:39:06 +0200

    add another function to the class

--- text_reader.py
+++ text_reader.py
@@ -1,8 +1,14 @@
+import os.path
 
 from FunFiles.file_reader import FileReader
 
 
 class TextReader(FileReader):
+    def check(self):
+        name = FileReader.name(self)
+        split_name = name.split(".")
+        if split_name[-1] == "txt":
+            return True
 
     def read(self):
         with open(self.file_path, "r") as my_file:
