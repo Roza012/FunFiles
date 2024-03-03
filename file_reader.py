a4b442b221ba87f5b58bd4beb98d5d3ee8233d56
Your Name <you@example.com>
2024-03-03 11:50:45 +0200

    made three functions(size,name,read)

--- file_reader.py
+++ file_reader.py
@@ -1,7 +1,20 @@
 import os
+from abc import abstractmethod
 
 
 class FileReader:
     def __init__(self, file_path):
         if os.path.isfile(file_path):
             self.file_path = file_path
+
+    def size(self):
+        file_size = os.stat(self.file_path).st_size
+        return file_size
+
+    def name(self):
+        file_name = os.path.basename(self.file_path)
+        return file_name
+
+    @abstractmethod
+    def read(self):
+        pass
